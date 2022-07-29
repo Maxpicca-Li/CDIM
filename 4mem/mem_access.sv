214f565bd6431a9c533c5adb6eefd2b4fcb092fb
Maxpicca-Li <liyanqin@cqu.edu.cn>
2022-07-29 22:34:04 +0800

    OPT: mem_access

--- 4mem/mem_access.sv
+++ 4mem/mem_access.sv
@@ -25,9 +25,6 @@ module mem_access (
 
     assign data_sram_en    = mem_en && ~(|M_master_except) && mem_addr != 32'hbfaffff0;
     assign data_sram_addr  = mem_addr;
-    // assign data_sram_addr = (mem_addr[31:28] == 4'hB) ? {4'h1, mem_addr[27:0]} :
-    //                          (mem_addr[31:28] == 4'h8) ? {4'h0, mem_addr[27:0]} :
-    //                           mem_addr;
                 
     always_comb begin:mem_access_transform
         ades = 1'b0; // 写指令地址错例外
@@ -40,115 +37,57 @@ module mem_access (
             `OP_LW: begin
                 data_sram_wen = 4'b0000;
                 data_sram_rlen = 2'd2;
-                if(mem_addr[1:0] != 2'b00) begin
-                    adel = 1'b1;
-                end
-                else begin
-                    mem_rdata = data_sram_rdata;
-                end
+                adel = mem_addr[1:0] != 2'b00;
+                mem_rdata = {32{mem_addr[1:0]==2'b00}} & data_sram_rdata;
             end
             `OP_LB: begin
                 data_sram_wen = 4'b0000;
                 data_sram_rlen = 2'd0;
-                case(mem_addr[1:0])
-                    2'b11:
-                        mem_rdata = {{24{data_sram_rdata[31]}},data_sram_rdata[31:24]};
-                    2'b10:
-                        mem_rdata = {{24{data_sram_rdata[23]}},data_sram_rdata[23:16]};
-                    2'b01:
-                        mem_rdata = {{24{data_sram_rdata[15]}},data_sram_rdata[15:8]};
-                    2'b00:
-                        mem_rdata = {{24{data_sram_rdata[7]}},data_sram_rdata[7:0]};
-                endcase
+                mem_rdata = {32{mem_addr[1:0]==2'b11}} & {{24{data_sram_rdata[31]}},data_sram_rdata[31:24]} |
+                            {32{mem_addr[1:0]==2'b10}} & {{24{data_sram_rdata[23]}},data_sram_rdata[23:16]} |
+                            {32{mem_addr[1:0]==2'b01}} & {{24{data_sram_rdata[15]}},data_sram_rdata[15: 8]} |
+                            {32{mem_addr[1:0]==2'b00}} & {{24{data_sram_rdata[ 7]}},data_sram_rdata[7 : 0]} ;
             end
             `OP_LBU: begin
                 data_sram_wen = 4'b0000;
                 data_sram_rlen = 2'd0;
-                case(mem_addr[1:0])
-                    2'b11:
-                        mem_rdata = {{24{1'b0}},data_sram_rdata[31:24]};
-                    2'b10:
-                        mem_rdata = {{24{1'b0}},data_sram_rdata[23:16]};
-                    2'b01:
-                        mem_rdata = {{24{1'b0}},data_sram_rdata[15:8]};
-                    2'b00:
-                        mem_rdata = {{24{1'b0}},data_sram_rdata[7:0]};
-                endcase
+                mem_rdata = {32{mem_addr[1:0]==2'b11}} & {{24{1'b0}},data_sram_rdata[31:24]} |
+                            {32{mem_addr[1:0]==2'b10}} & {{24{1'b0}},data_sram_rdata[23:16]} |
+                            {32{mem_addr[1:0]==2'b01}} & {{24{1'b0}},data_sram_rdata[15: 8]} |
+                            {32{mem_addr[1:0]==2'b00}} & {{24{1'b0}},data_sram_rdata[7 : 0]} ;
             end
             `OP_LH: begin
                 data_sram_wen = 4'b0000;
                 data_sram_rlen = 2'd1;
-                if(mem_addr[0] != 1'b0) begin
-                    adel = 1'b1;
-                end
-                else begin
-                    case(mem_addr[1])
-                        1'b1:
-                            mem_rdata = {{16{data_sram_rdata[31]}},data_sram_rdata[31:16]};
-                        1'b0:
-                            mem_rdata = {{16{data_sram_rdata[15]}},data_sram_rdata[15:0]};
-                    endcase
-                end
+                adel = mem_addr[0] != 1'b0;
+                mem_rdata = {32{mem_addr[1:0]==2'b10}} & {{16{data_sram_rdata[31]}},data_sram_rdata[31:16]} |
+                            {32{mem_addr[1:0]==2'b00}} & {{16{data_sram_rdata[15]}},data_sram_rdata[15: 0]} ;
             end
             `OP_LHU: begin
                 data_sram_wen = 4'b0000;
                 data_sram_rlen = 2'd1;
-                if(mem_addr[0] != 1'b0) begin
-                    adel = 1'b1;
-                end
-                else begin
-                    case(mem_addr[1])
-                        1'b1:
-                            mem_rdata = {{16{1'b0}},data_sram_rdata[31:16]};
-                        1'b0:
-                            mem_rdata = {{16{1'b0}},data_sram_rdata[15:0]};
-                    endcase
-                end
+                adel = mem_addr[0] != 1'b0;
+                mem_rdata = {32{mem_addr[1:0]==2'b10}} & {{16{1'b0}},data_sram_rdata[31:16]} |
+                            {32{mem_addr[1:0]==2'b00}} & {{16{1'b0}},data_sram_rdata[15: 0]} ;
             end
             `OP_SW: begin
-                if(mem_addr[1:0] != 2'b00) begin
-                    ades = 1'b1;
-                    data_sram_wen = 4'b0000;
-                end
-                else begin
-                    data_sram_wdata = mem_wdata;
-                    data_sram_wen =4'b1111;
-                end
+                ades = mem_addr[1:0] != 2'b00;
+                data_sram_wen = {4{mem_addr[1:0]==2'b00}} & 4'b1111;
+                data_sram_wdata = {32{mem_addr[1:0]==2'b00}} & mem_wdata;
             end
             `OP_SH: begin
-                if(mem_addr[0] != 1'b0) begin
-                    ades = 1'b1;
-                    data_sram_wen = 4'b0000;
-                end
-                else begin
-                    data_sram_wdata = {mem_wdata[15:0],mem_wdata[15:0]};
-                    case(mem_addr[1:0])
-                        2'b10:
-                            data_sram_wen = 4'b1100;
-                        2'b00:
-                            data_sram_wen = 4'b0011;
-                        default:
-                            ;
-                    endcase
-                end
+                ades = mem_addr[0] != 1'b0;
+                data_sram_wen = {4{mem_addr[1:0]==2'b10}} & 4'b1100 |
+                                {4{mem_addr[1:0]==2'b00}} & 4'b0011 ;
+                data_sram_wdata = {32{mem_addr[0]==1'b0}} & {mem_wdata[15:0],mem_wdata[15:0]};
             end
             `OP_SB: begin
                 data_sram_wdata = {mem_wdata[7:0],mem_wdata[7:0],mem_wdata[7:0],mem_wdata[7:0]};
-                case(mem_addr[1:0])
-                    2'b11:
-                        data_sram_wen = 4'b1000;
-                    2'b10:
-                        data_sram_wen = 4'b0100;
-                    2'b01:
-                        data_sram_wen = 4'b0010;
-                    2'b00:
-                        data_sram_wen = 4'b0001;
-                    default:
-                        ;
-                endcase
+                data_sram_wen = {4{mem_addr[1:0]==2'b11}} & 4'b1000 |
+                                {4{mem_addr[1:0]==2'b10}} & 4'b0100 |
+                                {4{mem_addr[1:0]==2'b01}} & 4'b0010 |
+                                {4{mem_addr[1:0]==2'b00}} & 4'b0001 ;
             end
-            default :
-                data_sram_wen = 4'b0000;
         endcase
     end
 
