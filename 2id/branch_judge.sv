c85998394f44fac0bd613920dd36503c1a9a2aa6
Maxpicca-Li <liyanqin@cqu.edu.cn>
2022-07-31 20:16:49 +0800

    OPT: branch_judge remove mux

--- 2id/branch_judge.sv
+++ 2id/branch_judge.sv
@@ -12,44 +12,23 @@ module branch_judge(
         output logic [31:0] pc_branch_address
 
     );
-
-    always_comb begin :branch_compute
-        if(branch_type == `BT_BEQ && rs_value==rt_value) begin
-            branch_taken = 1'b1;
-            pc_branch_address = pc_plus4 + offset;
-        end
-        else if(branch_type == `BT_BNE && rs_value!=rt_value) begin
-            branch_taken = 1'b1;
-            pc_branch_address = pc_plus4 + offset;
-        end
-        else if (branch_type ==`BT_BGTZ  && ((rs_value[31]==1'b0)&&(rs_value!=32'h0))) begin
-            branch_taken = 1'b1;
-            pc_branch_address = pc_plus4 + offset;
-        end
-        else if (branch_type ==`BT_BLEZ  && ((rs_value[31]==1'b1)||(rs_value==32'h0))) begin
-            branch_taken = 1'b1;
-            pc_branch_address = pc_plus4 + offset;
-        end
-        else if (branch_type ==`BT_BGEZ_ &&  rs_value[31]==1'b0) begin
-            branch_taken = 1'b1;
-            pc_branch_address = pc_plus4 + offset;
-        end
-        else if (branch_type ==`BT_BLTZ_ &&  rs_value[31]==1'b1) begin
-            branch_taken = 1'b1;
-            pc_branch_address = pc_plus4 + offset;
-        end
-        else if (branch_type ==`BT_J) begin
-            branch_taken = 1'b1;
-            pc_branch_address = {pc_plus4[31:28], j_target, 2'b00};
-        end
-        else if (branch_type == `BT_JREG) begin
-            branch_taken = 1'b1;
-            pc_branch_address = rs_value;
-        end
-        else begin // branch_type = `BT_NOP
-            branch_taken = 1'b0;
-            pc_branch_address = 32'hxxxxxxxx;
-        end
-    end
+    logic [31:0] pc_branch;
+    assign pc_branch = pc_plus4 + offset;
+    assign pc_branch_address =  {32{branch_type == `BT_BEQ   && rs_value==rt_value}}                        & pc_branch |
+                                {32{branch_type == `BT_BNE   && rs_value!=rt_value}}                        & pc_branch |
+                                {32{branch_type == `BT_BGTZ  && ((rs_value[31]==1'b0)&&(rs_value!=32'h0))}} & pc_branch |
+                                {32{branch_type == `BT_BLEZ  && ((rs_value[31]==1'b1)||(rs_value==32'h0))}} & pc_branch |
+                                {32{branch_type == `BT_BGEZ_ &&  rs_value[31]==1'b0}}                       & pc_branch |
+                                {32{branch_type == `BT_BLTZ_ &&  rs_value[31]==1'b1}}                       & pc_branch |
+                                {32{branch_type == `BT_JREG}}                                               & rs_value  |
+                                {32{branch_type == `BT_J}}                                                  & {pc_plus4[31:28], j_target, 2'b00};
+    assign branch_taken =   (branch_type == `BT_BEQ   && rs_value==rt_value) |
+                            (branch_type == `BT_BNE   && rs_value!=rt_value) |
+                            (branch_type == `BT_BGTZ  && ((rs_value[31]==1'b0)&&(rs_value!=32'h0))) |
+                            (branch_type == `BT_BLEZ  && ((rs_value[31]==1'b1)||(rs_value==32'h0))) |
+                            (branch_type == `BT_BGEZ_ &&  rs_value[31]==1'b0) |
+                            (branch_type == `BT_BLTZ_ &&  rs_value[31]==1'b1) |
+                            (branch_type == `BT_JREG) |
+                            (branch_type == `BT_J) ;
 
 endmodule
